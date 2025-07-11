bb7261eb469c62e10f85cd32468850948c073202
Mohan Lal <mhnuk2007@gmail.com>
2025-07-11 23:06:45 +0500

    Updated App logic and spring.xml to demonstrate autowire byName with explicit bean injection

--- src/main/java/com/learning/App.java
+++ src/main/java/com/learning/App.java
@@ -10,9 +10,7 @@ public class App {
         System.out.println(obj1.getAge());
         obj1.code();
 
-        Alien obj2 = (Alien) context.getBean("alien2");
-        System.out.println(obj2.getAge());
-        obj2.code();
+
 
 
     }
